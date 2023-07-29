92c37368e9b0768444d48d940b73e0c18dd69399
Luis Esteban Ramirez <lesteban@Luiss-Mac-mini.local>
2023-07-29 17:29:45 -0500

    add crew view

--- data.ts
+++ data.ts
@@ -40,37 +40,29 @@ export const destinations = [
 export const crew = [
   {
     name: "Douglas Hurley",
-    images: {
-      png: "./assets/crew/image-douglas-hurley.png",
-      webp: "./assets/crew/image-douglas-hurley.webp",
-    },
+    image:
+      "https://res.cloudinary.com/lesteban/image/upload/v1689982314/FM_space_tourism/image-douglas-hurley_yl5duv.png",
     role: "Commander",
     bio: "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
   },
   {
     name: "Mark Shuttleworth",
-    images: {
-      png: "./assets/crew/image-mark-shuttleworth.png",
-      webp: "./assets/crew/image-mark-shuttleworth.webp",
-    },
+    image:
+      "https://res.cloudinary.com/lesteban/image/upload/v1689982322/FM_space_tourism/image-mark-shuttleworth_ytcxvj.png",
     role: "Mission Specialist",
     bio: "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
   },
   {
     name: "Victor Glover",
-    images: {
-      png: "./assets/crew/image-victor-glover.png",
-      webp: "./assets/crew/image-victor-glover.webp",
-    },
+    image:
+      "https://res.cloudinary.com/lesteban/image/upload/v1689982328/FM_space_tourism/image-victor-glover_eab4uj.png",
     role: "Pilot",
     bio: "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.",
   },
   {
     name: "Anousheh Ansari",
-    images: {
-      png: "./assets/crew/image-anousheh-ansari.png",
-      webp: "./assets/crew/image-anousheh-ansari.webp",
-    },
+    image:
+      "https://res.cloudinary.com/lesteban/image/upload/v1689982303/FM_space_tourism/image-anousheh-ansari_sy9cpl.png",
     role: "Flight Engineer",
     bio: "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
   },
--- src/app/crew/page.tsx
+++ src/app/crew/page.tsx
@@ -1,7 +1,87 @@
+"use client";
+
 import React from "react";
+import {
+  Body,
+  H1,
+  H4,
+  HeaderLink,
+  Name,
+  SubHeading,
+  SubHeading2,
+  Title,
+  TitleNumber,
+} from "@/components/Typography";
+import Image from "next/image";
+import { crew } from "../../../data";
+import { GoDotFill } from "react-icons/go";
 
 const CrewPage = () => {
-  return <div>Crew Page</div>;
+  const [crewSelected, setCrewSelected] = React.useState(crew[0]);
+
+  setTimeout(() => {
+    const crewIndex = crew.indexOf(crewSelected);
+    if (crewIndex === crew.length - 1) {
+      setCrewSelected(crew[0]);
+    } else {
+      setCrewSelected(crew[crewIndex + 1]);
+    }
+  }
+  , 3000);
+
+  return (
+    <div className="flex flex-col items-center md:justify-between  gap-4 lg:flex-wrap  lg:w-6/12 lg:self-start lg:max-h-128 lg:fixed lg:left-0 lg:mt-4 lg:pl-32 lg:gap-36">
+      <span className="flex gap-2 md:self-start md:pl-8  ">
+        <TitleNumber>02</TitleNumber>
+        <Title>Meet your crew</Title>
+      </span>
+      <div className="w-full flex flex-col items-center justify-end min-h-imgContainer  md:order-last">
+        <Image
+          src={crewSelected.image}
+          alt={crewSelected.name}
+          width={177}
+          height={244}
+          className="md:hidden aspect-w-177 aspect-h-244"
+        />
+        <Image
+          src={crewSelected.image}
+          alt={crewSelected.name}
+          width={300}
+          height={300}
+          className="hidden md:block lg:hidden self-center"
+        />
+        <Image
+          src={crewSelected.image}
+          alt={crewSelected.name}
+          width={445}
+          height={445}
+          className="hidden lg:block aspect-w-445 aspect-h-445 ml-8 fixed bottom-0"
+        />
+        <hr className="w-10/12 border-1 border-gray-decorator mb-4 md:hidden " />
+      </div>
+      <ul className="flex gap-4 md:order-4 ">
+        {crew.map((memberTeam, index) => {
+          return (
+            <li key={index}>
+              <GoDotFill
+                className={`cursor-pointer ${
+                  memberTeam.name === crewSelected.name
+                    ? "text-white"
+                    : "text-gray-500"
+                }`}
+                onClick={() => setCrewSelected(crew[index])}
+              />
+            </li>
+          );
+        })}
+      </ul>
+      <div className="flex flex-col items-center px-4 gap-2 md:w-8/12 md:gap-8 lg:h-64 lg:items-start">
+        <H4>{crewSelected.role}</H4>
+        <Name>{crewSelected.name}</Name>
+        <Body>{crewSelected.bio}</Body>
+      </div>
+    </div>
+  );
 };
 
 export default CrewPage;
